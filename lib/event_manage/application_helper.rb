7c52e642e7f35c1d44db0894b7ae182b230423a5
Suguru Odai <ourqwers@gmail.com>
2013-12-21 23:44:01 +0900

    Fixed: #html_slice -> #truncate アルゴリズムも修正

--- lib/event_manage/application_helper.rb
+++ lib/event_manage/application_helper.rb
@@ -28,7 +28,7 @@ module EventManage
     def summary_escape(summary, words)
       # 文字型ではないとsizeが使えないので文字型に変換
       summary = summary.to_s
-      summary = html_slice(summary)
+      summary = truncate(summary)
       summary = h(summary)
       words = array_escape(words)
       summary = set_match_tag(summary, words)
@@ -49,10 +49,14 @@ module EventManage
       "q=#{params[:q]}&start_time=#{params[:start_time]}&end_time=#{params[:end_time]}"
     end
 
-    def html_slice(str)
-      return str if str.size <= 100
-      str = "#{str.slice(0...100)} ..."
-      str
+    def truncate(text, opts = {})
+      opts[:omission] ||= "..."
+      opts[:limit] ||= 100
+      if text.size > opts[:limit]
+        max_size = opts[:limit] - opts[:omission].size
+        text = text[0..max_size] + opts[:omission]
+      end
+      text
     end
 
     def set_match_tag(str, words)
