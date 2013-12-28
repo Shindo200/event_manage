b566f60b2d6a4f66c33da72b1723bf460b66c973
Suguru Odai <ourqwers@gmail.com>
2013-12-28 20:41:12 +0900

    Fixed: #snippet_*** を追加

--- lib/event_manage/application_helper.rb
+++ lib/event_manage/application_helper.rb
@@ -3,28 +3,33 @@ module EventManage
   module ApplicationHelper
     require 'uri'
     require 'rack'
+    require 'groonga'
     include Rack::Utils
     alias_method :h, :escape_html
 
-    def title_escape(title, words)
-      title = h(title)
-      words = array_escape(words)
-      title = set_match_tag(title, words)
-      title
+    def snippet_title(event, keywords)
+      snippet_defined_column(event, keywords, 30, :title)
     end
 
-    def summary_escape(summary, words)
-      # 文字型ではないとsizeが使えないので文字型に変換
-      summary = summary.to_s
-      summary = truncate(summary)
-      summary = h(summary)
-      words = array_escape(words)
-      summary = set_match_tag(summary, words)
-      summary
+    def snippet_summary(event, keywords)
+      snippet_defined_column(event, keywords, 100, :summary)
     end
 
-    def array_escape(ary)
-      ary.flatten.map {|str| h(str)}
+    def snippet_defined_column(event, keywords, width, column)
+      return "" if event[column].nil?
+      return event[column] if keywords.size == 0
+
+      snippet = Groonga::Snippet.new(
+        width:              width,
+        default_open_tag:   '<span class="match">',
+        default_close_tag:  '</span>',
+        html_escape:        true,
+        normalize:          true
+        )
+
+      keywords.each { |keyword| snippet.add_keyword(keyword) }
+
+      snippet.execute(event[column]).join
     end
 
     def escape_uri(str)
@@ -33,28 +38,6 @@ module EventManage
 
     def escape_query_params(params)
       escape_uri("q=#{params[:q]}&start_time=#{params[:start_time]}&end_time=#{params[:end_time]}")
-    end
-
-    def truncate(text, opts = {})
-      opts[:omission] ||= "..."
-      opts[:limit] ||= 100
-      if text.size > opts[:limit]
-        max_size = opts[:limit] - opts[:omission].size
-        text = text[0..max_size] + opts[:omission]
-      end
-      text
-    end
-
-    def set_match_tag(str, words)
-      # HACK: "\"で検索をかけるとエラーになるので暫定対応
-      begin
-        words.each do |word|
-          str = str.gsub(/(#{word})/i) {"\<span class=\"match\"\>#{$1}\<\/span\>"}
-        end
-        str
-      rescue
-        str
-      end
     end
   end
 end
