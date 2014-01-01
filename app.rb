c2fa59935a28212b0f2d9b2e0b91186fe9aa4fcf
Suguru Odai <ourqwers@gmail.com>
2014-01-01 12:46:48 +0900

    Fixed: "/:event_id/vote/up" の返り値は増やした後の vote 値になるように修正

--- app.rb
+++ app.rb
@@ -62,7 +62,9 @@ module EventManage
 
     post '/:event_id/vote/up' do
       # 対象のイベントの vote を1つ増やす
-      @database.open(DB_FILE_NAME) { |events| events.up_vote(params[:event_id]) }
+      vote = 0
+      @database.open(DB_FILE_NAME) { |events| vote = events.up_vote(params[:event_id]) }
+      vote
     end
 
     after do
