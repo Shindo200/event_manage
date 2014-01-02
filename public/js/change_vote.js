63f9d9cfa0c758138616c0d25f167dc771cacddd
Suguru Odai <ourqwers@gmail.com>
2014-01-02 22:31:36 +0900

    Added: up_vote ボタンを押したときに、vote 値を上げるリクエストを送る処理を追加

--- public/js/change_vote.js
+++ public/js/change_vote.js
@@ -1,2 +1,9 @@
 $(function() {
+  $('button.up_vote').click(function() {
+    var current_row = $(this).parents('tr');
+    var event_id = current_row.find('div.vote input[name=event_id]').val();
+    $.post('/' + event_id + '/vote/up', null, function(data) {
+      current_row.find('div.vote span.count_vote').text(data['vote'] + ' pt');
+    });
+  });
 });
