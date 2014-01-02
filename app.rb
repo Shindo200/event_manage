86d04a32bba9065605afb0a746aa08d2c975fdca
Suguru Odai <ourqwers@gmail.com>
2014-01-02 23:19:37 +0900

    Deleted: コメントアウトしていた検索時間計測処理を削除

--- app.rb
+++ app.rb
@@ -35,9 +35,6 @@ module EventManage
     get '/search' do
       redirect '/' unless params[:q]
 
-      # 検索にかかった時間を測りたい場合はコメントを外す
-      #start = Time.now
-
       @keywords = params[:q].gsub(/　/, ' ').split
       begin
         # TODO: ***_time が有効な文字列がチェックする処理を入れる
@@ -55,9 +52,6 @@ module EventManage
       @top_organizers = @events.get_top_organizer(5)
 
       @paged_events = @events.paginate(params[:page].to_i)
-
-      # 検索にかかった時間を測りたい場合はコメントを外す
-      #puts (Time.now - start).to_f
 
       haml :stats
     end
