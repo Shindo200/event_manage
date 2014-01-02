b95b2c94f02d31bc8f9e1fecba3374e5ee01a0dd
Suguru Odai <ourqwers@gmail.com>
2014-01-02 23:03:03 +0900

    Fixed: up_vote ボタンと down_vote ボタンを押したときの処理を一つの関数にまとめる

--- public/js/change_vote.js
+++ public/js/change_vote.js
@@ -1,17 +1,18 @@
 $(function() {
-  $('button.up_vote').click(function() {
-    var current_row = $(this).parents('tr');
-    var event_id = current_row.find('div.vote input[name=event_id]').val();
-    $.post('/' + event_id + '/vote/up', null, function(data) {
-      current_row.find('div.vote span.count_vote').text(data['vote'] + ' pt');
-    });
-  });
+  // vote 値を増やす
+  $('button.up_vote').on('click', { 'action': 'up' }, post_change_vote);
 
-  $('button.down_vote').click(function() {
+  // vote 値を減らす
+  $('button.down_vote').on('click', { 'action': 'down' }, post_change_vote);
+
+  function post_change_vote(e) {
     var current_row = $(this).parents('tr');
     var event_id = current_row.find('div.vote input[name=event_id]').val();
-    $.post('/' + event_id + '/vote/down', null, function(data) {
+    var url = '/' + event_id + '/vote/' + e.data.action;
+
+    $.post(url, null, function(data) {
+      // 更新後の vote 値に画面に表示する
       current_row.find('div.vote span.count_vote').text(data['vote'] + ' pt');
     });
-  });
+  }
 });
