beae5fef624ece3a051646f207013f78db05ee91
Suguru Odai <ourqwers@gmail.com>
2014-01-02 22:38:20 +0900

    up_vote ボタンを押したときに、vote 値を下げるリクエストを送る処理を追加

--- public/js/change_vote.js
+++ public/js/change_vote.js
@@ -6,4 +6,12 @@ $(function() {
       current_row.find('div.vote span.count_vote').text(data['vote'] + ' pt');
     });
   });
+
+  $('button.down_vote').click(function() {
+    var current_row = $(this).parents('tr');
+    var event_id = current_row.find('div.vote input[name=event_id]').val();
+    $.post('/' + event_id + '/vote/down', null, function(data) {
+      current_row.find('div.vote span.count_vote').text(data['vote'] + ' pt');
+    });
+  });
 });
