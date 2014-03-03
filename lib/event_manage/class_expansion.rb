c22e44ae6bf272469b835d3db82d7755c21c1e0f
Suguru Odai <ourqwers@gmail.com>
2014-03-03 13:51:40 +0900

    Add truncate method

--- lib/event_manage/class_expansion.rb
+++ lib/event_manage/class_expansion.rb
@@ -9,4 +9,15 @@ class String
     return false if self.size > 0
     true
   end
+
+  def truncate(opts = {})
+    opts[:omission] ||= "..."
+    opts[:limit] ||= 30
+    text = self
+    if text.size > opts[:limit]
+      max_size = opts[:limit] - opts[:omission].size
+      text = text[0..max_size] + opts[:omission]
+    end
+    text
+  end
 end
